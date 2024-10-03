9436536fb7f0f649de006472c8579412cd7fcc97
Vicente <vicenteferrer_01@hotmail.com>
2024-10-03 12:41:48 -0300

    Adapting for render.com

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,11 @@
-const { dirname } = import.meta;
+import { resolve } from 'node:path'
+
+const { dirname } = import.meta
 
 async function buildHome(req, res) {
-    const _dirname = dirname.split('\\').filter((_, i, arr) => i !== arr.length - 1).join('/');
+    const _dirname = dirname.split('\\').filter((_, i, arr) => i !== arr.length - 1).join('/')
 
-    res.sendFile(`${_dirname}/views/index.html`);
+    return res.sendFile(resolve(`${_dirname}/views/index.html`))
 }
 
 export { buildHome }
