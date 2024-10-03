2672249db95a1722cdc986c58236439cf110ce44
Vicente <vicenteferrer_01@hotmail.com>
2024-10-02 21:27:58 -0300

    Patching bugs

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,14 +3,13 @@ import { matchDateWithType, isInvalidDate } from '../utilities/helpers.js'
 
 async function getTimestamp(req, res) {
     const reqDate = matchDateWithType(req.params.date)
+    const date = isNaN(reqDate) && !reqDate ? Date.now() : reqDate
 
-    if (isInvalidDate(new Date(reqDate))) {
-        res.json({ error: 'Invalid Date' })
+    if (isInvalidDate(new Date(date))) {
+        return res.json({ error: 'Invalid Date' })
     }
 
-    const date = new Date(isNaN(reqDate) && !reqDate ? Date.now() : reqDate)
-
-    res.json(new Timestamp(date))
+    return res.json(new Timestamp(new Date(date)))
 }
 
 export { getTimestamp }
